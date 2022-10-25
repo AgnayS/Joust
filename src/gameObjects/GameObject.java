3d0590d0f1495435a7ea7f765e165fe0079b3025
greermm <greermm@RHIT-PW01EGC0.rose-hulman.edu>
2022-10-25 09:53:55 -0400

    In GameObject, added implementation to collision(), getters, constructor

--- src/gameObjects/GameObject.java
+++ src/gameObjects/GameObject.java
@@ -4,13 +4,49 @@ import java.awt.Image;
 
 public abstract class GameObject {
 
-	protected int width, height;
-	
-	protected double xPos, yPos, xVel, yVel;
-	Image sprite;
+	protected final int width, height;
+	protected final Image sprite;
 
-	public boolean collision() {
-		return true;
+	protected double xPos, yPos, xVel, yVel;
+
+	public GameObject(int width, int height, Image sprite, double xPos, double yPos) {
+		this.width = width;
+		this.height = height;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.sprite = sprite;
 	}
 
+	public boolean collision(GameObject other) {
+		return (this.xPos + this.width >= other.getxPos() || other.getxPos() + other.getWidth() >= this.xPos)
+				&& (this.yPos + this.height >= other.getyPos() || other.getyPos() + other.getHeight() >= this.yPos);
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public double getxPos() {
+		return xPos;
+	}
+
+	public double getyPos() {
+		return yPos;
+	}
+
+	public double getxVel() {
+		return xVel;
+	}
+
+	public double getyVel() {
+		return yVel;
+	}
+
+	public Image getSprite() {
+		return sprite;
+	}
 }
